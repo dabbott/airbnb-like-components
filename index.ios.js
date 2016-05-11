510aafb5c1002862d3997346763d1a5a1f1931af
Devin Abbott <devinabbott@gmail.com>
2016-05-11 12:44:17 -0700

    Index from the demo video

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,16 @@
+import {
+  Hero,
+  ActionButton,
+  Marquee,
+  Divider,
+  Gallery,
+} from './components'
 import React, { Component } from 'react'
 import {
   View,
   AppRegistry,
   ScrollView,
+  Dimensions,
 } from 'react-native'
 
 class Project extends Component {
@@ -15,7 +23,28 @@ class Project extends Component {
     return (
       <View style={style}>
         <ScrollView>
-
+          <Hero 
+            image={{uri: 'https://qanvast-api.s3.amazonaws.com/ware/original/12a4ba3a-5ab7-4f04-937b-d8be5e3bc2f7.jpg'}}
+          />
+          <ActionButton 
+            color={"rgba(255,101,106,1)"}
+            side={"left"}
+            icon={"search"}
+          />
+          <Marquee 
+            title={'Hi, Devin'}
+            subtitle={'Welcome to Deco, the easiest way to put together a React Native app.'}
+          />
+          <Divider />
+          <Gallery
+            width={Dimensions.get('window').width}
+            title={'Just for the weekend'}
+            items={[
+              {title: 'Los Angeles', image: {uri: 'https://unsplash.it/600/400/?image=100'}},
+              {title: 'San Francisco', image: {uri: 'https://unsplash.it/600/400/?image=200'}},
+              {title: 'New York', image: {uri: 'https://unsplash.it/600/400/?image=300'}},
+            ]}
+          />
         </ScrollView>
       </View>
     );
